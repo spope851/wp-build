c00332881583fb839ae5926762cb7b36825b1fd2
Spencer Pope <spenpo@spenpo.com>
2025-08-12 17:10:43 +0800

    correct table name in migration

--- migrations/activate-hello-dolly.sql
+++ migrations/activate-hello-dolly.sql
@@ -3,15 +3,15 @@
 -- Note: Hello Dolly comes with WordPress core, so we just need to activate it
 
 -- Get current active plugins (using the same prefix as migrations table)
-SET @current_active_plugins = (SELECT option_value FROM dfs_options WHERE option_name = 'active_plugins' LIMIT 1);
+SET @current_active_plugins = (SELECT option_value FROM wp_options WHERE option_name = 'active_plugins' LIMIT 1);
 
 -- If no active plugins exist, create the option
-INSERT INTO dfs_options (option_name, option_value, autoload) 
+INSERT INTO wp_options (option_name, option_value, autoload) 
 SELECT 'active_plugins', 'a:1:{s:12:"hello-dolly/hello.php";s:0:"";}', 'yes'
-WHERE NOT EXISTS (SELECT 1 FROM dfs_options WHERE option_name = 'active_plugins');
+WHERE NOT EXISTS (SELECT 1 FROM wp_options WHERE option_name = 'active_plugins');
 
 -- If active plugins exist, add hello-dolly to the list
-UPDATE dfs_options 
+UPDATE wp_options 
 SET option_value = CASE 
     WHEN option_value = '' OR option_value IS NULL THEN 'a:1:{s:12:"hello-dolly/hello.php";s:0:"";}'
     WHEN option_value NOT LIKE '%hello-dolly%' THEN CONCAT(SUBSTRING(option_value, 1, LENGTH(option_value)-1), ',s:12:"hello-dolly/hello.php";s:0:"";}')
